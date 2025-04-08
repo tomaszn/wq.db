e1109c0fdc2a6a08396667b18cb41084ce4f2ade
Tomasz Nowak <tomaszn@users.noreply.github.com>
2025-04-08 20:24:14 +0200

    update ci

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
     ],
     test_suite='tests',
     setup_requires=[
-        'setuptools_scm',
+        'setuptools_scm<8',
     ],
     project_urls={
         'Homepage': 'https://wq.io/wq.db/',
